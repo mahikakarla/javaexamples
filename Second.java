435cd66e9e6b78c5f69eec4ecf7787c0c0456854
mahikakarla <mahithakakarla@gmail.com>
2021-02-23 11:05:30 -0500

    updated second java file

--- Second.java
+++ Second.java
@@ -1,5 +1,5 @@
 public class Second {
   public static void main(String[] args){
-    System.out.println("This is my first program in java");
+    System.out.println("This is my third program in java");
   }//End of main
 }//End of FirstJavaProgram Class
